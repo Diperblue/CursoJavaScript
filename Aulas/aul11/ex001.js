8e04ffd0c9fa0fdb1d085fe7c815ff3479396360
Jorge Guimarães Martins <jorgeguimaraesdev@gmail.com>
2021-02-15 16:36:25 -0300

    Aula 11

---
+++ Aulas/aul11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu e ${vel}Km/m`)
+if (vel > 60) { //Condição simples
+    console.log('Você foi MULTADO!!')
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
---
+++ Aulas/aul11/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log (`Vivendo no ${pais}`)
+if (pais == 'BRASIL') {
+    console.log('Você e Brasileiro!!')
+} else {
+    console.log('Você e estrangeiro!!')
+}
